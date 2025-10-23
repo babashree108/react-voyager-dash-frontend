87d385e259783f23e6b66b6e137181a17b61820c
shivamraj <shivamraj.sj01@gmail.com>
2025-10-24 02:25:49 +0530

    add teacher backend module basic CRUD

---
+++ backend/src/main/java/com/nxtclass/controller/TeacherDetailsController.java
@@ -0,0 +1,59 @@
+package com.nxtclass.controller;
+
+import com.nxtclass.dto.TeacherDetailsDTO;
+import com.nxtclass.service.impl.TeacherDetailsAPI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/teacher-details")
+public class TeacherDetailsController {
+    private final TeacherDetailsAPI teacherDetailsAPI;
+
+    @GetMapping("list")
+    public ResponseEntity<List<TeacherDetailsDTO>> list() {
+        List<TeacherDetailsDTO> teachers = teacherDetailsAPI.list();
+        return ResponseEntity.ok(teachers);
+    }
+
+    @GetMapping("count")
+    public ResponseEntity<Long> count() {
+        return ResponseEntity.ok(teacherDetailsAPI.list().stream().count());
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Long> save(@RequestBody TeacherDetailsDTO dto) {
+        return ResponseEntity.ok(teacherDetailsAPI.save(dto));
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<?> update(@RequestBody TeacherDetailsDTO dto) {
+        return (dto.getIdentifier() == null)
+                ? ResponseEntity.badRequest().body("Identifier is required for update.")
+                : ResponseEntity.ok(teacherDetailsAPI.save(dto));
+    }
+
+    @GetMapping("/{identifier}")
+    public ResponseEntity<?> getDetails(@PathVariable Long identifier) {
+        try {
+            TeacherDetailsDTO dto = teacherDetailsAPI.details(identifier);
+            return ResponseEntity.ok(dto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{identifier}")
+    public ResponseEntity<String> delete(@PathVariable Long identifier) {
+        try {
+            String result = teacherDetailsAPI.delete(identifier);
+            return ResponseEntity.ok(result);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ backend/src/main/java/com/nxtclass/entity/TeacherDetails.java
@@ -0,0 +1,24 @@
+package com.nxtclass.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TeacherDetails extends BaseDomain{
+    private String fName;
+    private String lName;
+    private String email;
+    private String phoneNo;
+    private String address1;
+    private String address2;
+    private Long pincode;
+    private String state;
+    private String country;
+    private String adharNo;
+}
